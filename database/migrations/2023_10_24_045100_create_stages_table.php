589e0c8f64a2a3b15a0fd8fe6be69462e6e0ca26
or15012 <143147788+or15012@users.noreply.github.com>
2023-10-27 22:39:02 -0600

    arreglando migración

--- database/migrations/2023_10_24_045100_create_stages_table.php
+++ database/migrations/2023_10_24_045100_create_stages_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->foreignId('protocol_id')->constrained('users')->onDelete('restrict');
-            $table->foreignId('cyvle_id')->constrained('groups')->onDelete('restrict');
+            $table->foreignId('cycle_id')->constrained('cycles')->onDelete('restrict');
             $table->timestamps();
         });
     }
