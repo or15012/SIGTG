da7b2238174850400287df054f660588cc0aed3b
abrego8 <ar14019@ues.edu.sv>
2023-11-11 23:28:03 -0600

    Lista y adjuntar documento a etapa

---
+++ resources/views/evaluations_documents/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.master')
+@section('title')
+    @lang('translation.Dashboard')
+@endsection
+
+@section('content')
+    @component('components.breadcrumb')
+        @slot('li_1')
+            SIGTG-FIA
+        @endslot
+        @slot('title')
+            Welcome !
+        @endslot
+    @endcomponent
+    <div class="container">
+        <h1>Agregar etapa</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('evaluations_documents.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
+            </div>
+            <div class="mb-3">
+                <label for="path" class="form-label">Archivo etapa</label>
+                <input type="file" class="form-control" id="path" name="path" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+    </div>
+@endsection
+
+@section('script')
+    <script src="{{ URL::asset('assets/js/app.js') }}"></script>
+@endsection
