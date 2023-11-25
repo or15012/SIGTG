0a713d54b7332b1431116f88a316b83794e9e04f
or15012 <143147788+or15012@users.noreply.github.com>
2023-11-25 09:40:56 -0600

    arreglando permisos

--- resources/views/mail/extension-updated.blade.php
+++ resources/views/mail/extension-updated.blade.php
@@ -4,13 +4,13 @@
     <div style="padding: 20px;">
         <h1 style="color: #333;">Prorroga modificada con éxito!</h1>
 
-        <p>Hola {{ $emailData['user']->name }},</p>
+        <p>Hola {{ $Info['user']->name }},</p>
 
-        <p>Se ha modificado una prórroga para el proyecto "{{ $emailData['project']->name }}". Aquí están los detalles:</p>
+        <p>Se ha modificado una prórroga para el proyecto "{{ $Info['project']->name }}". Aquí están los detalles:</p>
 
         <strong>Detalles de la prórroga:</strong>
         <ul>
-            <li><strong>Descripción:</strong> {{ $emailData['extension']->description }}</li>
+            <li><strong>Descripción:</strong> {{ $Info['extension']->description }}</li>
         </ul>
 
 
