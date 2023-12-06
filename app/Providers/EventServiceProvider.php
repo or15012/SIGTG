59ae7d2285607bd9b56d81605502e81ae25e9d78
or15012 <143147788+or15012@users.noreply.github.com>
2023-12-05 21:10:04 -0600

    avance session

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\School;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,6 +28,22 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen(\Illuminate\Auth\Events\Login::class, function ($event) {
+            // Este código se ejecutará después de que un usuario se haya autenticado.
+            $user = $event->user;
+
+            $school = School::find($user->school_id);
+
+            //que pasa sino tiene escuela asignada
+
+            //tengo que revisar si esque no tiene o si posee todas segun el rol
+            $schoolReturn = array(
+                "id"    => $school->id,
+                "name"  => $school->name,
+            );
+
+            // Puedes asignar tu variable de sesión aquí.
+            session(['school' => $schoolReturn]);
+        });
     }
 }
