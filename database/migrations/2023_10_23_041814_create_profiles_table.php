be4d7486ee5c520cfc38830619ff888630ba967e
abrego8 <ar14019@ues.edu.sv>
2024-01-02 22:11:28 -0600

    Nullable para prioridad de pre perfil por planificaciones, arreglo de bug en fecha created

--- database/migrations/2023_10_23_041814_create_profiles_table.php
+++ database/migrations/2023_10_23_041814_create_profiles_table.php
@@ -18,11 +18,11 @@ return new class extends Migration
             $table->string("name");
             $table->longText("description");
             $table->boolean("type");
-            $table->string("path")->nullable()->default(null);;
-            $table->string("vision_path")->nullable()->default(null);;
-            $table->string("summary_path")->nullable()->default(null);;
-            $table->string("size_calculation_path")->nullable()->default(null);;
-            $table->integer('proposal_priority');
+            $table->string("path")->nullable()->default(null);
+            $table->string("vision_path")->nullable()->default(null);
+            $table->string("summary_path")->nullable()->default(null);
+            $table->string("size_calculation_path")->nullable()->default(null);
+            $table->integer('proposal_priority')->nullable()->default(null);
             $table->integer("status");
             $table->foreignId('group_id')->constrained('groups')->onDelete('restrict');
             $table->timestamps();
