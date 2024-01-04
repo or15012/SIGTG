39cdd4c27fab3cdedddd0a91b2fc2214c6710786
Celina <xelaju_celina@yahoo.com>
2024-01-03 23:40:08 -0600

    Tabla Propuestas

---
+++ database/migrations/2024_01_03_165921_create_proposal_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposal', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->mediumText("description")->nullable()->default(null);
+            $table->string("path")->nullable()->default(null);;
+            $table->integer("cant_est");
+            $table->foreignId('entity_id')->constrained('entities')->onDelete('restrict');
+            $table->timestamps();
+            $table->softDeletes();
+
+            //Restricción de unicidad
+            $table->unique(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal');
+    }
+};
