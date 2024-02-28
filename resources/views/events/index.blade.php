dd5c4e3b1da69de033cf7f40323c528bdbd1aaf4
Celina <xelaju_celina@yahoo.com>
2024-02-27 18:16:10 -0600

    Update index.blade.php

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -60,7 +60,7 @@
                         <td>{{ $event->place }}</td>
                         <td>{{ $event->description }}</td>
                         <td>
-                            <a href="{{ route('events.edit', ['events' => $event->id, 'project' => $project->id]) }}class="btn btn-primary"><i
+                            <a href="{{ route('events.edit', ['events' => $event->id, 'project' => $project->id]) }}"class="btn btn-primary"><i
                                 class="fas fa-pen"></i></a>
                         </td>
                     </tr>
