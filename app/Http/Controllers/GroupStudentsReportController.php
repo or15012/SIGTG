609eb14d731b4898c36640ae5c9cda9a59ee48dc
Celina <xelaju_celina@yahoo.com>
2024-03-19 23:39:36 -0600

    alguito

---
+++ app/Http/Controllers/GroupStudentsReportController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Group;
+use App\Models\Cycle;
+use App\Models\EvaluationStage;
+use App\Models\EvaluationStageNote;
+use App\Models\Stage;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Concerns\FromArray;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Illuminate\Support\Facades\Response;
+
+class GroupStudentsReportController extends Controller
+{
+
+}
