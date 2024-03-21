5441ae75a26550bbcc4ae675b868a6240514e454
Celina <xelaju_celina@yahoo.com>
2024-03-20 23:45:48 -0600

    Update web-dashboard.php

--- routes/web-dashboard.php
+++ routes/web-dashboard.php
@@ -53,7 +53,7 @@ Route::group(['prefix' => 'dashboards', 'as' => 'dashboards.'], function () {
 
 
 //Grupo de rutas para reporte de cursos
-Route::group(['prefix' => 'reports', 'as' => 'reports.'], function () {
+Route::group(['prefix' => 'reports', 'as' => 'coursereports.'], function () {
     Route::get('/', [StudentsCousesReportController::class, 'index'])->name('index');
     Route::get('/protocol/cycle/{id?}',[StudentsCousesReportController::class, 'ajaxProto'])->name('proto');
     Route::get('/course/cycle/{id?}',[StudentsCousesReportController::class, 'ajaxCourse'])->name('course');
@@ -65,7 +65,7 @@ Route::group(['prefix' => 'reports', 'as' => 'reports.'], function () {
 });
 
 //Grupo de rutas para reporte de grupos (integrantes)
-Route::group(['prefix' => 'reports', 'as' => 'reports.'], function () {
+Route::group(['prefix' => 'reports', 'as' => 'groupreports.'], function () {
     Route::get('/', [GroupStudentsReportController::class, 'index'])->name('index');
     Route::get('/protocol/cycle/{id?}',[GroupStudentsReportController::class, 'ajaxProto'])->name('proto');
     Route::get('/course/cycle/{id?}',[GroupStudentsReportController::class, 'ajaxCourse'])->name('course');
