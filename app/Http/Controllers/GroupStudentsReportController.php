b2ac4b52a5885ef93118bb923620fcb6db93ba4b
Celina <xelaju_celina@yahoo.com>
2024-03-20 22:34:28 -0600

    Update GroupStudentsReportController.php

--- app/Http/Controllers/GroupStudentsReportController.php
+++ app/Http/Controllers/GroupStudentsReportController.php
@@ -121,122 +121,80 @@ class GroupStudentsReportController extends Controller
 
     public function ajaxExcelCourses($cycle_id)
     {
-        $datos = DB::table('projects as pj')
-            ->select(
-                'pt.name as protocol_name',
-                'sch.name as name_school',
-                'gr.number as group_number',
-                'pj.name as project_name',
-                'us.first_name as name_student',
-                'us.middle_name as second_name_student',
-                'us.last_name as last_name_student',
-                'us.second_last_name as second_last_name_student',
-                'us.carnet as carnet_student',
-                'pj.status'
-            )
-            ->join('groups AS gr', 'gr.id', 'pj.group_id')
-            ->join('protocols as pt', 'pt.id', 'gr.protocol_id')
-            ->join('user_group as ug', 'ug.group_id', 'gr.id')
-            ->join('user_protocol as up', 'up.protocol_id', 'pt.id')
-            ->join('users as us', 'us.id', 'up.user_id')
-            ->join('schools as sch', 'us.school_id', 'sch.id')
-            ->groupBy(
-                'pt.name',
-                'sch.name',
-                'gr.number',
-                'pj.name',
-                'pj.status',
-                'us.first_name',
-                'us.middle_name',
-                'us.last_name',
-                'us.second_last_name',
-                'us.carnet'
-            ) // Agregar todas las columnas select en GROUP BY
-            ->orderBy('pj.status');
+        $inscritosProtocoloCuatro = DB::table('groups as gro')
+            ->join('cycles as cy', 'cy.id', '=', 'gro.cycle_id')
+            ->join('protocols as proto', 'proto.id', '=', 'gro.protocol_id')
+            ->join('user_protocol as up', 'up.protocol_id', '=', 'proto.id')
+            ->join('users as u', 'u.id', '=', 'up.user_id')
+            ->join('schools as sch', 'u.school_id', 'sch.id')
+            ->leftJoin('course_registrations as cour', function ($join) {
+                $join->on('cour.user_id', '=', 'up.user_id')
+                    ->on('cour.course_id', '=', 'gro.protocol_id');
+            })
+            ->where('u.type', 1)
+            ->where('u.state', 1)
+            ->where('proto.id', 4)
+            ->where('cy.status', 1);
+    
         if (session('school')['id'] != -1) {
-            $datos->where('us.school_id', session('school')['id']);
+            $inscritosProtocoloCuatro->where('u.school_id', session('school')['id']);
         }
-        $datos->where('pt.id', session('protocol')['id']);
-        $datos->where('gr.cycle_id', '=', $cycle_id);
-
-        $datos = $datos->get();
-
-        // Convertir los estados a texto
-        foreach ($datos as $dato) {
-            if ($dato->status == 1) {
-                $dato->status_text = 'Iniciado';
-            } elseif ($dato->status == 2) {
-                $dato->status_text = 'En proceso';
-            } elseif ($dato->status == 3) {
-                $dato->status_text = 'Finalizado';
-            } else {
-                $dato->status_text = ''; // Manejar otro caso si es necesario
-            }
+    
+        if (isset(session('protocol')['id']) && session('protocol')['id'] != -1) {
+            $inscritosProtocoloCuatro->where('proto.id', session('protocol')['id']);
         }
-
-        $data =  json_decode(json_encode($datos), true);
-
+    
+        $inscritosProtocoloCuatro = $inscritosProtocoloCuatro->get();
+    
+        $todosProtocoloCuatro = DB::table('groups as gro')
+            ->join('cycles as cy', 'cy.id', '=', 'gro.cycle_id')
+            ->join('protocols as proto', 'proto.id', '=', 'gro.protocol_id')
+            ->join('user_protocol as up', 'up.protocol_id', '=', 'proto.id')
+            ->join('users as u', 'u.id', '=', 'up.user_id')
+            ->join('schools as sch', 'u.school_id', 'sch.id')
+            ->where('u.type', 1)
+            ->where('u.state', 1)
+            ->where('proto.id', 4)
+            ->where('cy.status', 1);
+    
+        if (session('school')['id'] != -1) {
+            $todosProtocoloCuatro->where('u.school_id', session('school')['id']);
+        }
+    
+        if (isset(session('protocol')['id']) && session('protocol')['id'] != -1) {
+            $todosProtocoloCuatro->where('proto.id', session('protocol')['id']);
+        }
+    
+        $todosProtocoloCuatro = $todosProtocoloCuatro->count();
+    
+        $noInscritosProtocoloCuatro = $todosProtocoloCuatro - count($inscritosProtocoloCuatro);
+    
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-
-        $rowIndex = 1; // Comenzar desde la primera fila
-
-        $currentSchool = ''; // Variable para rastrear la escuela actual
-        foreach ($datos as $row) {
-            if ($row->name_school !== $currentSchool) {
-                // Agregar el nombre de la escuela con formato y estilo
-                $sheet->getStyle('A' . $rowIndex)->getFont()->setBold(true);
-                $sheet->setCellValue('A' . $rowIndex, 'Nombre de escuela: ' . $row->name_school);
-                $sheet->mergeCells('A' . $rowIndex . ':E' . $rowIndex); // Fusionar celdas para la escuela
-                $sheet->getStyle('A' . $rowIndex)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT); // Alinear a la izquierda
-                $currentSchool = $row->name_school;
-                $rowIndex++;
-            }
-
-            // Agregar el nombre del protocolo con formato y estilo
-            $sheet->getStyle('A' . $rowIndex)->getFont()->setBold(true);
-            $sheet->setCellValue('A' . $rowIndex, 'Nombre de protocolo: ' . $row->protocol_name);
-            $sheet->mergeCells('A' . $rowIndex . ':E' . $rowIndex); // Fusionar celdas para el protocolo
-            $sheet->getStyle('A' . $rowIndex)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT); // Alinear a la izquierda
-            $rowIndex++;
-
-            // Añadir encabezados de columnas debajo del tema
-            $sheet->setCellValue('A' . $rowIndex, 'Número de grupo');
-            $sheet->setCellValue('B' . $rowIndex, 'Nombre de proyecto');
-            $sheet->setCellValue('C' . $rowIndex, 'Estado de proyecto');
-            $sheet->setCellValue('D' . $rowIndex, 'Nombres de estudiante');
-            $sheet->setCellValue('E' . $rowIndex, 'Apellidos de estudiante');
-            $sheet->setCellValue('F' . $rowIndex, 'CARNET');
-            $rowIndex++;
-
-            // Añadir datos correspondientes
-            $sheet->setCellValue('A' . $rowIndex, $row->group_number);
-            $sheet->setCellValue('B' . $rowIndex, $row->project_name);
-            $sheet->setCellValue('C' . $rowIndex, $row->status_text); // Usar el nuevo campo 'status_text' en lugar de 'status'
-            $sheet->setCellValue('D' . $rowIndex, $row->name_student . ' ' . $row->second_name_student);
-            $sheet->setCellValue('E' . $rowIndex, $row->last_name_student . ' ' . $row->second_last_name_student);
-            $sheet->setCellValue('F' . $rowIndex, $row->carnet_student);
-            $rowIndex++;
-        }
-
+    
+        $sheet->setCellValue('A1', 'Estado');
+        $sheet->setCellValue('B1', 'Cantidad');
+    
+        $sheet->setCellValue('A2', 'Inscritos');
+        $sheet->setCellValue('B2', count($inscritosProtocoloCuatro));
+    
+        $sheet->setCellValue('A3', 'No Inscritos');
+        $sheet->setCellValue('B3', $noInscritosProtocoloCuatro);
+    
         // Establecer anchos de columna (opcional)
         $sheet->getColumnDimension('A')->setWidth(20);
-        $sheet->getColumnDimension('B')->setWidth(50);
-        $sheet->getColumnDimension('C')->setWidth(25);
-        $sheet->getColumnDimension('D')->setWidth(40);
-        $sheet->getColumnDimension('E')->setWidth(40);
-        $sheet->getColumnDimension('F')->setWidth(40);
-
-
+        $sheet->getColumnDimension('B')->setWidth(20);
+    
         // Crear un objeto de escritura
         $writer = new Xlsx($spreadsheet);
-
+    
         // Guardar el archivo en un directorio temporal
-        $filename = 'projects.xlsx';
+        $filename = 'inscritos_no_inscritos.xlsx';
         $writer->save($filename);
-
+    
         return response()->file($filename, ['Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']);
     }
+    
 
 
 
